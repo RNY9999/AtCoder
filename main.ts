d21e7c78283e7f3da9e4a4ad9c82ac48777e21e0
renya-hisamatsu <rennya090611@gmail.com>
2026-04-25 20:59:34 +0900

    AtCorder用 TS 実行環境の完成

---
+++ main.ts
@@ -0,0 +1,19 @@
+import * as readline from 'node:readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const lines: string[] = [];
+
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+rl.on('close', () => {
+  solution();
+});
+
+const solution = () => {
+};
